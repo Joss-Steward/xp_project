f4f797ce49a6282aa67bfc1021bd39552c7bb7fc
ShipsMerlin <merlin@cs.ship.edu>
2013-09-11 08:03:56 -0400

    Comments . . .

--- GameClient/src/model/QualifiedObserver.java
+++ GameClient/src/model/QualifiedObserver.java
@@ -2,6 +2,7 @@
 package model;
 
 /**
+ * This interface is required of all objects that want to observe QualifiedObservables
  * @author Merlin
  *
  */
@@ -9,8 +10,9 @@ public interface QualifiedObserver
 {
 
 	/**
-	 * @param obs
-	 * @param string
+	 * This method will be called when the QualifiedObservable we are watching changes state
+	 * @param arg a (possibly null) object describing what has changed
+	 * @param obs the QualifiedObservable that is notifying
 	 */
 	void update(QualifiedObservable obs, Object arg);
 }
