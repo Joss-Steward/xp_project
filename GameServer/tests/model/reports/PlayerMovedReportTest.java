e1a2ca4a832b02e2a4f939fb3ca57cd668c5844e
ShipsMerlin <merlin@cs.ship.edu>
2013-09-28 16:48:49 -0400

    Cleanup and improve test coverage

---
+++ GameServer/tests/model/reports/PlayerMovedReportTest.java
@@ -0,0 +1,37 @@
+package model.reports;
+
+import static org.junit.Assert.*;
+import nl.jqno.equalsverifier.EqualsVerifier;
+
+import org.junit.Test;
+
+import data.Position;
+
+
+/**
+ * @author Merlin
+ *
+ */
+public class PlayerMovedReportTest
+{
+
+	/**
+	 * make sure it gets built correctly
+	 */
+	@Test
+	public void creation()
+	{
+		PlayerMovedReport report = new PlayerMovedReport(33, new Position(3,4));
+		assertEquals(33, report.getUserID());
+		assertEquals(new Position(3,4), report.getNewPosition());
+	}
+
+	/**
+	 * Make sure the equals contract is obeyed
+	 */
+	@Test
+	public void equalsContract()
+	{
+		EqualsVerifier.forClass(PlayerMovedReport.class).verify();
+	}
+}
