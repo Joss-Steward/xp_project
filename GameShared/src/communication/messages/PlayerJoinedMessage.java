1725f9f5c78b0d4a57b0e3dbb0d2e4641d701de7
ShipsMerlin <merlin@cs.ship.edu>
2014-02-27 08:25:04 -0500

    Get Player's position and appearance type from the database all the way to the message packers on the server

--- GameShared/src/communication/messages/PlayerJoinedMessage.java
+++ GameShared/src/communication/messages/PlayerJoinedMessage.java
@@ -2,6 +2,8 @@ package communication.messages;
 
 import java.io.Serializable;
 
+import data.Position;
+
 /**
  * Sent to all clients when a new player connects to an area server
  * @author Merlin
@@ -16,15 +18,21 @@ public class PlayerJoinedMessage implements Message, Serializable
 	private static final long serialVersionUID = 1L;
 	private final String playerName;
 	private int playerID;
+	private String appearanceType;
+	private Position position;
 
 	/**
 	 * @param playerID the unique ID of the player
 	 * @param playerName the name of the new player
+	 * @param position where this player is on the map on this server
+	 * @param appearanceType the way the player should be drawn on the screen
 	 */
-	public PlayerJoinedMessage(int playerID, String playerName)
+	public PlayerJoinedMessage(int playerID, String playerName, String appearanceType, Position position)
 	{
 		this.playerID = playerID;
 		this.playerName = playerName;
+		this.appearanceType = appearanceType;
+		this.position = position;
 	}
 
 	/**
@@ -51,5 +59,23 @@ public class PlayerJoinedMessage implements Message, Serializable
 	public String toString()
 	{
 		return "PlayerJoined Message: playerName = " + playerName;
+	}
+
+	/**
+	 * Get the appearance type that shows how this player wants to be displayed
+	 * @return the appearance type
+	 */
+	public String getAppearanceType()
+	{
+		return appearanceType;
+	}
+
+	/**
+	 * Get the position this player is in
+	 * @return the position
+	 */
+	public Position getPosition()
+	{
+		return position;
 	}
 }
