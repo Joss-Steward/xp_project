a959926a699fc6eb886ce113ca43548d057e5e75
mk3969 <mk3969@mct165s12.cs.ship.edu>
2014-04-07 18:55:39 -0400

    Created the ChatMessageHandler

---
+++ GameClient/src/communication/handlers/ChatMessageHandler.java
@@ -0,0 +1,44 @@
+package communication.handlers;
+
+import model.CommandChatMessageReceived;
+import model.ModelFacade;
+import communication.messages.ChatMessage;
+import communication.messages.Message;
+
+/**
+ * Should process an incoming ChatMessage that is reporting that a message was sent by a Player
+ * 
+ * @author Frank Schmidt
+ *
+ */
+public class ChatMessageHandler extends MessageHandler 
+{
+	
+	/** 
+	 * Passes a ChatMessage to the ChatMessageReceivedCommand
+	 * 
+	 * @see MessageHandler#process(Message)
+	 */
+	@Override
+	public void process(Message msg) 
+	{
+		if (msg.getClass().equals(ChatMessage.class))
+		{
+			ChatMessage chatMessage = (ChatMessage) msg;
+			CommandChatMessageReceived cmd = new CommandChatMessageReceived(chatMessage.getSenderName(), chatMessage.getMessage(), 
+					chatMessage.getPosition(), chatMessage.getType());
+			ModelFacade.getSingleton().queueCommand(cmd);
+		}
+		
+	}
+
+	/**
+	 * @see communication.handlers.MessageHandler#getMessageTypeWeHandle()
+	 */
+	@Override
+	public Class<?> getMessageTypeWeHandle() 
+	{
+		return ChatMessage.class;
+	}
+
+}
