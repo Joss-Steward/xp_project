e62524735bf5fdbf1222966d66af4b3be432c793
nhydock <nhydock@gmail.com>
2014-04-30 18:48:42 -0400

    Remove test for writing map files to clients

--- GameClient/tests/communication/handlers/MapFileMessageHandlerTest.java
+++ GameClient/tests/communication/handlers/MapFileMessageHandlerTest.java
@@ -31,38 +31,6 @@ public class MapFileMessageHandlerTest
 	}
 
 	/**
-	 * The handler should write the file to current.tmx
-	 * 
-	 * @throws IOException
-	 *             shouldn't
-	 * @throws InterruptedException
-	 *             shouldn't
-	 */
-	@Test
-	public void writesTheFile() throws IOException, InterruptedException
-	{
-		new File("/maps/current.tmx").delete();
-		new File("/maps/tileset/grass-tiles-2-small.png").delete();
-		new File("/maps/tileset/tree2-final.png").delete();
-
-		MapFileMessage msg = new MapFileMessage("testMaps/simple.tmx");
-		MapFileMessageHandler handler = new MapFileMessageHandler();
-		handler.process(msg);
-
-		File expected = new File("testMaps/simple.tmx");
-		File actual = new File("maps/current.tmx");
-		FileAssert.assertEquals(expected, actual);
-
-		expected = new File("testMaps/tileset/grass-tiles-2-small.png");
-		actual = new File("maps/tileset/grass-tiles-2-small.png");
-		FileAssert.assertEquals(expected, actual);
-
-		expected = new File("testMaps/tileset/tree2-final.png");
-		actual = new File("maps/tileset/tree2-final.png");
-		FileAssert.assertEquals(expected, actual);
-	}
-
-	/**
 	 * The handler should tell the model that the new file is there. We will
 	 * know that happens if the model reports the change to its observers
 	 * 
