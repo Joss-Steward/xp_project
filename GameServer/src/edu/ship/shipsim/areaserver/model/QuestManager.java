bb4c8f535c96bad8e865d2091cbe93b4df8be51a
ld0417 <ld0417@cs.ship.edu>
2015-02-25 16:52:52 -0500

    Added two adventures for a quest 2 Adding functionality for QuestManager Adding tests for Quest Manager in QuestManagerTest

---
+++ GameServer/src/edu/ship/shipsim/areaserver/model/QuestManager.java
@@ -0,0 +1,67 @@
+package edu.ship.shipsim.areaserver.model;
+
+import java.util.ArrayList;
+
+import datasource.DatabaseException;
+import model.Quest;
+import model.Adventure;
+import edu.ship.shipsim.areaserver.datasource.AdventureRecord;
+import edu.ship.shipsim.areaserver.datasource.AdventureTableDataGatewayMock;
+import edu.ship.shipsim.areaserver.datasource.QuestRowDataGatewayMock;
+
+/**
+ * Retrieves the list of quest and adventures from the database
+ * and sends them to the PlayerManager?
+ * @author lavonne
+ */
+public class QuestManager 
+{
+
+	/**
+	 * The method returns a singleton of QuestManager
+	 * @return the only QuestManager in the system
+	 */
+	public synchronized static QuestManager getSingleton()
+	{
+		if(singleton == null)
+		{
+			singleton = new QuestManager();
+		}
+		
+		return singleton;
+	}
+	
+	/**
+	 * Reset the singleton to null
+	 */
+	public static void resetSingleton()
+	{
+		if(singleton != null)
+		{
+			singleton = null;
+		}
+	}
+	
+	private static QuestManager singleton;
+
+	/**
+	 * Gets a specific quest from the database. Creates a Quest and passes it on the player mapper?
+	 * @param questID the quest to retrieve
+	 * @return quest the retrieved request
+	 * @throws DatabaseException
+	 */
+	public Quest getQuest(int questID) throws DatabaseException {
+		
+		QuestRowDataGatewayMock mock = new QuestRowDataGatewayMock(questID);
+		AdventureTableDataGatewayMock adventureMock = new AdventureTableDataGatewayMock();
+		ArrayList<Adventure> adventureList = new ArrayList<Adventure>();
+				
+		for(AdventureRecord ar : adventureMock.getAdventuresForQuest(questID))
+		{
+			adventureList.add(new Adventure(ar.getAdventureID(), ar.getAdventureDescription()));
+		}
+		
+		Quest quest = new Quest(mock.getQuestID(), mock.getQuestDescription(), adventureList);
+		return quest;
+	}
+}
