8f45ee858a1a1da2759c04bdb0ca50b5f5694cc3
Ethan <Ethan@157.160.176.111>
2015-03-16 16:47:30 -0400

    Comments

--- GameShared/src/communication/messages/CurrentQuestStateMessage.java
+++ GameShared/src/communication/messages/CurrentQuestStateMessage.java
@@ -4,17 +4,29 @@ import java.util.ArrayList;
 
 import model.ClientPlayerQuest;
 
+/**
+ * @author Merlin
+ *
+ */
 public class CurrentQuestStateMessage implements Message
 {
 
 	
 	private ArrayList<ClientPlayerQuest> clientPlayerQuestList;
 
+	/**
+	 * Constructor for Quest State Message
+	 * @param clientPlayerQuestList players quest list
+	 */
 	public CurrentQuestStateMessage(ArrayList<ClientPlayerQuest> clientPlayerQuestList)
 	{
 		this.clientPlayerQuestList = clientPlayerQuestList;
 	}
 
+	/**
+	 * Return current players quest List
+	 * @return quest list
+	 */
 	public ArrayList<ClientPlayerQuest> getClientPlayerQuestList()
 	{
 		return clientPlayerQuestList;
