5a0dedb1275787c4a2a988d31867ab49e1b464fe
ShipsMerlin <merlin@cs.ship.edu>
2015-04-22 15:33:20 -0400

    Groundwork for PlayerTableDataGateway preparing for retrieving high scores

---
+++ GameServer/src/edu/ship/shipsim/areaserver/datasource/PlayerTableDataGateway.java
@@ -0,0 +1,12 @@
+package edu.ship.shipsim.areaserver.datasource;
+
+public class PlayerTableDataGateway
+{
+
+	public void resetData()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ GameServer/tests/edu/ship/shipsim/areaserver/datasource/PlayerTableDataGatewayTest.java
@@ -0,0 +1,66 @@
+package edu.ship.shipsim.areaserver.datasource;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Test;
+
+import datasource.DatabaseException;
+import datasource.DatabaseTest;
+import datasource.QuestStateEnum;
+
+/**
+ * An abstract class that tests the table data gateways into the Adventure table
+ * 
+ * @author merlin
+ *
+ */
+public abstract class PlayerTableDataGatewayTest extends DatabaseTest
+{
+
+	private PlayerTableDataGateway gateway;
+
+	/**
+	 * Make sure any static information is cleaned up between tests
+	 */
+	@After
+	public void cleanup()
+	{
+		if (gateway != null)
+		{
+			gateway.resetData();
+		}
+	}
+
+	/**
+	 * @return the gateway we should test
+	 */
+	public abstract PlayerTableDataGateway getGatewaySingleton();
+
+	/**
+	 * 
+	 */
+	@Test
+	public void isASingleton()
+	{
+		PlayerTableDataGateway x = getGatewaySingleton();
+		PlayerTableDataGateway y = getGatewaySingleton();
+		assertSame(x, y);
+		assertNotNull(x);
+	}
+
+	/**
+	 * @throws DatabaseException
+	 *             shouldn't
+	 */
+	@Test
+	public void retrieveTopTenHighScores() throws DatabaseException
+	{
+		gateway = getGatewaySingleton();
+		
+	}
+
+	
+}
