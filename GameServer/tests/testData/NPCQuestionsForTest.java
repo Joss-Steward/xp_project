d2905e05136c853363189438f9311ed02469264d
ChrisHersh <ch5968@cs.ship.edu>
2016-03-23 18:37:43 -0400

    Added start and end dates to questions

--- GameServer/tests/testData/NPCQuestionsForTest.java
+++ GameServer/tests/testData/NPCQuestionsForTest.java
@@ -1,5 +1,9 @@
 package testData;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Date;
+
 /**
  * Create test questions for the DB.
  * 
@@ -11,12 +15,15 @@ public enum NPCQuestionsForTest
 	/**
 	 * 
 	 */
-	ONE(1, "First question", "First answer"),
+	ONE(1, "First question", "First answer", new GregorianCalendar(2014, Calendar.FEBRUARY, 11).getTime(), 
+	new GregorianCalendar(2017, Calendar.MARCH, 21).getTime()),
+	
 
 	/**
 	 * 
 	 */
-	TWO(2, "Second question", "Second answer");
+	TWO(2, "Second question", "Second answer", new GregorianCalendar(2014, Calendar.FEBRUARY, 11).getTime(),
+	        new GregorianCalendar(2026, Calendar.FEBRUARY, 11).getTime());
 
 	private String q;
 
@@ -24,6 +31,11 @@ public enum NPCQuestionsForTest
 
 	private int questionID;
 
+	private Date startDate;
+	
+	private Date endDate;
+	
+	
 	/**
 	 * Constructor
 	 * 
@@ -33,12 +45,18 @@ public enum NPCQuestionsForTest
 	 *            question
 	 * @param a
 	 *            answer
+     * @param startDate
+     *            first day the question is available
+     * @param endDate
+     *            last day the question is available
 	 */
-	NPCQuestionsForTest(int questionID, String q, String a)
+	NPCQuestionsForTest(int questionID, String q, String a, Date startDate, Date endDate)
 	{
 		this.questionID = questionID;
 		this.q = q;
 		this.a = a;
+		this.startDate = startDate;
+		this.endDate = endDate;
 	}
 	/**
 	 * @return the answer
@@ -62,6 +80,22 @@ public enum NPCQuestionsForTest
 	public int getQuestionID()
 	{
 		return questionID;
+	}
+	
+	/**
+	 * @return the first day the question is available
+	 */
+	public Date getStartDate()
+	{
+	    return startDate;
+	}
+	
+	/**
+	 * @return the last day the question is available
+	 */
+	public Date getEndDate()
+	{
+	    return endDate;
 	}
 	
 
